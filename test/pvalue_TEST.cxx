c9160dacbe2f135852b769f8dbd18192ac523b74
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2017-04-07 17:56:30 +0200

    [test] Lower pvalue accuracy

--- test/pvalue_TEST.cxx
+++ test/pvalue_TEST.cxx
@@ -17,7 +17,7 @@ TEST(pvalue_test, mathematica)
     auto T = {2., 5., 10., 20., 50.};
     auto P = {0.8936721808595665, 0.42457437866154357, 0.06934906413527009,
               0.0014159488909252227, 9.575188641974819e-9};
-    auto eps = {1e-15, 1e-15, 1e-15, 1e-17, 1e-22};
+    auto eps = {1e-15, 1e-15, 1e-15, 1e-15, 1e-15};
     for (auto t = T.begin(), p = P.begin(), e=eps.begin(); t != T.end(); ++t, ++p, ++e)
         EXPECT_NEAR(runs_pvalue(*t, n), *p, *e);
 }
